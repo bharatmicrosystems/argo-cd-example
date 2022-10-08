3c695cb1a37529a3287bf132504258ca8651f27a
Paul Silcock <ps88uk@gmail.com>
2022-10-08 16:54:09 +0100

    Work around GKE endpoint not being available

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,7 +68,7 @@ resource "google_container_node_pool" "main_spot_nodes" {
 }
 
 resource "time_sleep" "wait_30_seconds" {
-  depends_on = [google_container_node_pool.main_spot_nodes]
+  depends_on = [google_container_cluster.main]
   create_duration = "30s"
 }
 
